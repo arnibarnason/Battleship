df190b301d04f8b03db9ef43157bb6a1c82e5a5a
Arni B <arnibarnason@gmail.com>
2015-05-15 12:34:17 +0000

    fix

--- Battleship.py
+++ Battleship.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from Tkinter import Tk, W, E, PhotoImage, Label, RAISED
+from Tkinter import Tk, W, E, Label, RAISED
 from ttk import Frame, Style
 from ttk import Entry
+from time import sleep
+import Player
 
 class Battleship(Frame):
 
@@ -17,21 +19,42 @@ class Battleship(Frame):
 
         self.parent.title("Battleship")
 
+        player1 = Player()
+        player2 = ComputerPlayer()
+
         for i in range(10):
             self.columnconfigure(i)
             self.rowconfigure(i)
+        
+        for i in range(11):
+            self.columnconfigure(i+10)
 
         entry = Entry(self)
         for i in range(10):
             for j in range(10):
                 k = (i,j)
-                x = Label(self, relief=RAISED, width=8, height=3, padx=3, pady=3)
-                x.bind("<Button-1>", self.callback)
+                x = Label(self, relief=RAISED, width=4, height=2)
+                x.bind("<Button-1>", lambda event, arg=k: self.callback(event, arg))
                 x.grid(row=i, column=j)
+
+        for i in range(10):
+            x = Label(self, width=2, height=2, background="black")
+            x.grid(column=10, row=i)
+        
+        for i in range(10):
+            for j in range(11,21):
+                k = (i,j)
+                x = Label(self, relief=RAISED, width=4, height=2)
+                #x.bind("<Button-1>", lambda event, arg=k: self.callback(event, arg))
+                x.grid(row=i, column=j)
+
         self.pack()
 
-    def callback(self, event):
-        event.widget["background"] = "red"
+    def callback(self, event, pos):
+        color = "red"#player2.underAttack(pos)
+        event.widget["background"] = color
+        time.sleep(3)
+        player1.underAttack()
 
 def main():
 
