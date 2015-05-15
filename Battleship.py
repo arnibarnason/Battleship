9747b6537c5fbc546d4f5789442a0c17c28a4c6d
Arni B <arnibarnason@gmail.com>
2015-05-15 15:27:00 +0000

    Changed structure of battleship.py

--- Battleship.py
+++ Battleship.py
@@ -5,6 +5,7 @@ from ttk import Frame, Style
 from ttk import Entry
 from time import sleep
 import Player
+from Cell import Cell
 
 class Battleship(Frame):
 
@@ -34,25 +35,25 @@ class Battleship(Frame):
                 x = Label(self, relief=RAISED, width=4, height=2)
                 x.bind("<Button-1>", lambda event, arg=k: self.callback(event, arg))
                 x.grid(row=i, column=j)
+                cell = Cell(False, False, x)
+                player2.addCellToOcean(cell, i)
 
         for i in range(10):
             x = Label(self, width=2, height=2, background="black")
             x.grid(column=10, row=i)
-        
+
         for i in range(10):
             for j in range(11,21):
-                k = (i,j)
                 x = Label(self, relief=RAISED, width=4, height=2)
-                #x.bind("<Button-1>", lambda event, arg=k: self.callback(event, arg))
                 x.grid(row=i, column=j)
-
+                cell = Cell(False, False, x)
+                player1.addCellToOcean(cell, i)
         self.pack()
 
     def callback(self, event, pos):
-        color = self.player2.underAttack(pos)
-        event.widget["background"] = color
-        #sleep(3)
-        #self.player1.underAttack()
+        self.player2.underAttack(pos)
+        sleep(3)
+        self.player1.underAttack()
 
 def main():
 
