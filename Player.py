cc2cfb634b8c0e0c6b8818a13cf6debef0bbbc34
Magnús Sigurðarson <maggisig92@gmail.com>
2015-05-17 16:53:36 +0000

    ai finished?

--- Player.py
+++ Player.py
@@ -45,30 +45,56 @@ class Player():
 	#try to make a smart move based on previous hits:
 	def aI(self):
 		print("inside ai")	
+		#t is what will be returned
+		t = []
 		#if this is the first time we hit this particular ship:
-		if len(self.hits) == 1 and not self.target:
+		if len(self.hits) == 1:
 			x = self.hits[0][0]
 			y = self.hits[0][1]
-			#initialize target to surrounding coordinates:
-			self.target = [(x+1,y),(x-1,y), (x,y+1), (x,y-1)]
-			print("clean target:",[t for t in [(x+1,y),(x-1,y), (x,y+1), (x,y-1)] if not self.ocean[t[0]][t[1]].isHit])
-	
-			for t in self.target:
-				print("t line 54:",t)
-				self.target.remove(t)
-				if self.ocean[t[0]][t[1]].isShip:
-					print("t inside if: ",t)
-					return t
+			self.boundaryCheck()
+			#initialize target to surrounding coordinates that are not already hit:
+			self.target = [t for t in [(x+1,y),(x-1,y), (x,y+1), (x,y-1)] if not self.ocean[t[0]][t[1]].isHit]
+			t = self.target[0]
+			print("t line 55:",t)
+			self.target.remove(t)
+			return t
 		#here we have hit ship more than once
 		else:
-			'''if we still have targets:
-			if self.target:
-				for i in self.target:
-					if 
-			return (x,y)'''
 			#ship lies vertically:
-			#if hits[0][0] == hits[1][0]
+			if self.hits[0][0] == self.hits[1][0]:
+			#set target to opposite ends of previous hit-line
+				firstHit = self.hits[0]
+				lastHit = self.hits[-1]
+				if firstHit > lastHit:
+					self.target = [(firstHit[0],firstHit[1]+1), (lastHit[0],lastHit[1]-1)]
+				else:
+					self.target = [(firstHit[0],firstHit[1]-1), (lastHit[0],lastHit[1]+1)]
+			else:
+				#ship lies horizontally
+				firstHit = self.hits[0]
+				lastHit = self.hits[-1]
+				if firstHit > lastHit:
+					self.target = [(firstHit[0]+1,firstHit[1]), (lastHit[0]-1,lastHit[1])]
+				else: 
+					self.target = [(firstHit[0]-1,firstHit[1]), (lastHit[0]+1,lastHit[1])]
+			self.boundaryCheck()
+			#remove targets that have already been hit, might be unnecessary
+			self.target = [t for t in self.target if not self.ocean[t[0]][t[1]].isHit]
+			if not self.target:
+			#we have already tried all potential targets 
+				self.hits = []
+				return (randint(0,9),randint(0,9))
+			t = self.target[0]
+			self.target.remove(t)
+		return t
 				
+	def boundaryCheck(self):
+		#check if target is inside of ocean, if it is a valid index
+		for t in self.target:
+			if t[0] not in range(10):
+				self.target.remove(t)
+
+
 	def setUpShips(self):
                 ships = [2,3,3,4,5]
                 for i in ships:
